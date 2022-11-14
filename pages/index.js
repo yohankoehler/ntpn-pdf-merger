321890ce93c1bc75586a59b49f8542ad58cf0735
yohan.koehler <yohan.koehler@MBP-X44QVFTXG6.leboncoin.loc>
2022-11-14 11:40:20 +0100

    feat(adlife): update style dans charlist

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,17 @@
 import Head from "next/head";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
 import Merger from "@components/Merger";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Merger
-          files={[
-            // "http://localhost:3000/pdfs/K.pdf",
-            // `${window.location.protocol}//${window.location.host}/pdfs/L.pdf`,
-          ]}
-        />
+        <Merger files={[]} />
       </main>
-
-      <Footer />
     </div>
   );
 }
