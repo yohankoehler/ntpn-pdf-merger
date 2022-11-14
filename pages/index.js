4749899930a66ac0bbd32cd454a7ed45b1e1a397
yohan <yohan@MacBook-Pro-de-yohan.local>
2022-11-14 18:47:24 +0100

    change page title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Collage PDF maker NT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
